234011eb155032aad491fb5b6f42dea7e6c46f65
codewithash <134801667+codewithash-hub@users.noreply.github.com>
2024-10-25 11:59:58 +0200

    ad

---
+++ Symptom-Checker/symptom_checker_project/symptom_checker/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+default_user = User.objects.first()
+
+# Create your models here.
+class Interaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=default_user.id)
+    user_input = models.TextField() 
+    ai_response = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} Interaction at {self.timestamp} - User input: {self.user_input[:50]}"
+    
+    class Meta:
+        ordering = ['-timestamp']
